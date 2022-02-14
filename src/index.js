040305422f82e40d9a475a9d1644116238e6ae01
lehongquocvuong <vuongd20084@cusc.ctu.edu.vn>
2022-02-14 11:27:44 +0700

    Lesson 2 Q3

--- src/index.js
+++ src/index.js
@@ -60,65 +60,85 @@ const name = "Your name";
 // );
 
 //_______LESSON 2,[Bài tập] Tạo Element thể hiện bảng thông tin các sinh viên trong lớp học
-const students = [
-  {
-    company: "Alfreds Futterkiste",
-    contact: "Maria Anders",
-    country: "Germany",
-  },
-  {
-    company: "Centro comercial Moctezuma",
-    contact: "Francisco Chang",
-    country: "Mexico",
-  },
-  {
-    company: "Ernst Handel",
-    contact: "Roland Mendel",
-    country: "Austria",
-  },
-  {
-    company: "Island Trading",
-    contact: "Helen Bennett",
-    country: "UK",
-  },
-  {
-    company: "Laughing Bacchus Winecellars",
-    contact: "Yoshi Tannamuri",
-    country: "Canada",
-  },
-  {
-    company: "Magazzini Alimentari Riuniti",
-    contact: "Giovanni Rovelli",
-    country: "Italy",
-  },
-];
+// const students = [
+//   {
+//     company: "Alfreds Futterkiste",
+//     contact: "Maria Anders",
+//     country: "Germany",
+//   },
+//   {
+//     company: "Centro comercial Moctezuma",
+//     contact: "Francisco Chang",
+//     country: "Mexico",
+//   },
+//   {
+//     company: "Ernst Handel",
+//     contact: "Roland Mendel",
+//     country: "Austria",
+//   },
+//   {
+//     company: "Island Trading",
+//     contact: "Helen Bennett",
+//     country: "UK",
+//   },
+//   {
+//     company: "Laughing Bacchus Winecellars",
+//     contact: "Yoshi Tannamuri",
+//     country: "Canada",
+//   },
+//   {
+//     company: "Magazzini Alimentari Riuniti",
+//     contact: "Giovanni Rovelli",
+//     country: "Italy",
+//   },
+// ];
 
+// ReactDOM.render(
+//   <div>
+//     <h2>Students</h2>
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Company</th>
+//           <th>Contact</th>
+//           <th>Country</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {students.map((student) => (
+//           <tr>
+//             <td>{student.company}</td>
+//             <td>{student.contact}</td>
+//             <td>{student.country}</td>
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+//_______LESSON 2, [Bài tập] Tạo Element thể hiện Profile Card (như hình minh hoạ)
 ReactDOM.render(
-  <div>
-    <h2>Students</h2>
-    <table>
-      <thead>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-      </thead>
-      <tbody>
-        {students.map((student) => (
-          <tr>
-            <td>{student.company}</td>
-            <td>{student.contact}</td>
-            <td>{student.country}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  <div className="container">
+    <div className="card">
+      <div className="card--header" />
+      <img
+        className="avatar"
+        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        alt="avatar"
+      />
+      <div className="card--body">
+        <div>
+          <p className="text-header">Lê Hồng Quốc Vương</p>
+          <p className="text-sub">Xin chào!</p>
+          <button className="btn">FOLLOW</button>
+        </div>
+      </div>
+    </div>
   </div>,
   document.getElementById("root")
 );
-
-//_______LESSON 2,
 
 //_______LESSON 2,
 
