cc02dcdc2bddfb3001a1c6670dd58c91600fa0ad
lehongquocvuong <vuongd20084@cusc.ctu.edu.vn>
2022-02-14 11:32:53 +0700

    Lesson 2 Q4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 // Import file index.css vào file index.js
 import "./index.css";
 
+import "bootstrap/dist/css/bootstrap.css";
+
 const name = "Your name";
 
 //______LESSON 2, TOPIC 4
@@ -119,27 +121,70 @@ const name = "Your name";
 // );
 
 //_______LESSON 2, [Bài tập] Tạo Element thể hiện Profile Card (như hình minh hoạ)
+// ReactDOM.render(
+//   <div className="container">
+//     <div className="card">
+//       <div className="card--header" />
+//       <img
+//         className="avatar"
+//         src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+//         alt="avatar"
+//       />
+//       <div className="card--body">
+//         <div>
+//           <p className="text-header">Lê Hồng Quốc Vương</p>
+//           <p className="text-sub">Xin chào!</p>
+//           <button className="btn">FOLLOW</button>
+//         </div>
+//       </div>
+//     </div>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+//_______LESSON 2, [Bài tập] Tạo Element thể hiện Sign in Form sử dụng Bootstrap
+
 ReactDOM.render(
-  <div className="container">
-    <div className="card">
-      <div className="card--header" />
-      <img
-        className="avatar"
-        src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        alt="avatar"
-      />
-      <div className="card--body">
-        <div>
-          <p className="text-header">Lê Hồng Quốc Vương</p>
-          <p className="text-sub">Xin chào!</p>
-          <button className="btn">FOLLOW</button>
+  <div className="container d-flex align-items-center text-center">
+    <div className="form-signin">
+      <form>
+        <img
+          className="mb-4"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png"
+          alt=""
+          width="72"
+          height="57"
+        />
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+        <div className="form-floating">
+          <input
+            type="email"
+            className="form-control email"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label>Email address</label>
         </div>
-      </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control password"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label>Password</label>
+        </div>
+        <div className="checkbox mb-3">
+          <label>
+            <input type="checkbox" /> Remember me
+          </label>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+        <p className="mt-5 mb-3 text-muted">© 2022</p>
+      </form>
     </div>
   </div>,
   document.getElementById("root")
 );
-
-//_______LESSON 2,
-
-//_______LESSON 2,
