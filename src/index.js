9c1fbce47207e8a4f614007c31f1cd5ddcd08d89
lehongquocvuong <vuongd20084@cusc.ctu.edu.vn>
2022-02-14 11:20:36 +0700

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,125 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// Import file index.css vào file index.js
+import "./index.css";
+
+const name = "Your name";
+
+//______LESSON 2, TOPIC 4
+// ReactDOM.render(
+//   React.createElement("h1", { style: { textAlign: "center" } }, name),
+//   document.getElementById("root")
+// );
+
+//______LESSON 2, TOPIC 5
+// ReactDOM.render(
+//   <h1 style={{ textAlign: "center" }}>{name}</h1>,
+//   document.getElementById("root")
+// );
+
+//______LESSON 2, TOPIC 6
+// const fruits = [
+//   "Apple",
+//   "Banana",
+//   "Orange",
+//   "Apricot",
+//   "Black rowan",
+//   "Cranberry",
+// ];
+
+// ReactDOM.render(
+//   <div>
+//     <h1>List of fruits</h1>
+//     <ul>
+//       {fruits.map((item) => (
+//         <li>{item}</li>
+//       ))}
+//     </ul>
+//   </div>,
+//   document.getElementById("root")
+// );
+
+//_______LESSON 2, TOPIC 7
+
+// const tick = () => {
+//   ReactDOM.render(
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}</h2>
+//     </div>,
+//     document.getElementById("root")
+//   );
+// };
+
+// setInterval(tick, 1000);
+
+//_______LESSON 2, [Bài tập] Tạo Element thể hiện thông tin của Trình duyệt bạn đang sử dụng
+// ReactDOM.render(
+//   <h4>Browser's details: {navigator.userAgent}</h4>,
+//   document.getElementById("root")
+// );
+
+//_______LESSON 2,[Bài tập] Tạo Element thể hiện bảng thông tin các sinh viên trong lớp học
+const students = [
+  {
+    company: "Alfreds Futterkiste",
+    contact: "Maria Anders",
+    country: "Germany",
+  },
+  {
+    company: "Centro comercial Moctezuma",
+    contact: "Francisco Chang",
+    country: "Mexico",
+  },
+  {
+    company: "Ernst Handel",
+    contact: "Roland Mendel",
+    country: "Austria",
+  },
+  {
+    company: "Island Trading",
+    contact: "Helen Bennett",
+    country: "UK",
+  },
+  {
+    company: "Laughing Bacchus Winecellars",
+    contact: "Yoshi Tannamuri",
+    country: "Canada",
+  },
+  {
+    company: "Magazzini Alimentari Riuniti",
+    contact: "Giovanni Rovelli",
+    country: "Italy",
+  },
+];
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <h2>Students</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+      </thead>
+      <tbody>
+        {students.map((student) => (
+          <tr>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//_______LESSON 2,
+
+//_______LESSON 2,
+
+//_______LESSON 2,
